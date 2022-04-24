644ffe9ed9e49100f705af69d58d02f79f928c39
yawam <93334358+yawam@users.noreply.github.com>
2022-04-23 23:09:00 -0600

    finished

--- index script/main.js
+++ index script/main.js
@@ -4,10 +4,8 @@ console.log(date);
 let year = date.getFullYear();
 console.log(year);
 
-document.querySelector('h1').textContent = year;
+document.querySelector('footer','p').textContent = `${year} | Papa Yaw Agyeman-Manu| Accra`;
 
-document.querySelector('div').innerHTML = '<h2>Subtitle</h2>';
-
-let newpara = document.createElement('p');
-newpara.textContent = 'This is my paragraph';
-document.querySelector('div').appendChild(newpara)
+let currentdate = document.lastModified;
+// document.querySelector('span').textContent = currentdate;
+console.log(currentdate);
