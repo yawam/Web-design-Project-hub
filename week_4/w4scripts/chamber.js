0d03b6840fa919a26ca814930ef1835c6b20f375
yawam <93334358+yawam@users.noreply.github.com>
2022-05-16 07:03:31 -0600

    date format changes

--- week_4/w4scripts/chamber.js
+++ week_4/w4scripts/chamber.js
@@ -1,4 +1,20 @@
 "use strict"
+
+const date = new Date();
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+console.log(date.toLocaleDateString('en-us', options));
+document.querySelector("#date").textContent = date.toLocaleDateString('en-us', options);
+
+const year = date.getFullYear();
+document.querySelector('#year').textContent = year;
+console.log(year);
+
+let currentdate = document.lastModified;
+document.querySelector('#lastmod').textContent = currentdate;
+console.log(currentdate);
+
+// creating toggle function
 function toggleMenu(){
     document.querySelector("#primarynav").classList.toggle("open");
     document.querySelector("#hamgurgerbtn").classList.toggle("open");
