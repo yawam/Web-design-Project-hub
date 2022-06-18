df0884da27e53cade9dfaadd90c1d9d2b2027179
yawam <93334358+yawam@users.noreply.github.com>
2022-06-18 13:29:40 -0600

    json png to jpg

--- week_9/scripts/directory.js
+++ week_9/scripts/directory.js
@@ -1,1 +1,27 @@
-const requestURL = "https://github.com/yawam/WDD-230/blob/master/week_9/scripts/data.json"
+const requestURL = "https://yawam.github.io/WDD-230/week_9/scripts/data.json";
+
+async function getComps(URL){
+    const response = await fetch(URL);
+    if(response.ok){
+        const jsonObject =await response.json();
+        const company = jsonObject["Companies"];
+        company.forEach(displayComps);
+
+
+    }
+};
+
+getComps(requestURL);
+
+function displayComps(object){
+    let card = document.createElement("section");
+    let image = document.createElement("img");
+    let name = document.createElement("p");
+    let address = document.createElement("p");
+    let phone = document.createElement("p");
+    let website = document.createElement("p");
+
+    image.setAttribute("src", object.image);
+    image.setAttribute("alt", "Company Logo");
+
+};
