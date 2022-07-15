4d2b7349acaba9f1ceedd89792875a92370a2333
yawam <93334358+yawam@users.noreply.github.com>
2022-07-14 23:06:37 -0600

    Temple inn and suites test

--- week_9/scripts/directory.js
+++ week_9/scripts/directory.js
@@ -3,7 +3,7 @@ const requestURL = "https://yawam.github.io/WDD-230/week_9/scripts/data.json";
 async function getComps(URL){
     const response = await fetch(URL);
     if(response.ok){
-        const jsonObject =await response.json();
+        const jsonObject = await response.json();
         const company = jsonObject["Companies"];
         company.forEach(displayComps);
         company.forEach(displayList);
